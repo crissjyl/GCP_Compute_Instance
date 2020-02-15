ecd5866b769997d2b1ac21de8b59c674ee88be60
crissjyl <cliu713@gmail.com>
2020-02-15 13:58:05 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ variable "us-west2-a" {
 
 provider "google" {
  credentials  = "${file(var.gcp_credentials)}"
-  project     = "${var.gcp_project}"
+  project     = "var.gcp_project"
   region      = "${var.gcp_region}"
 
 }
